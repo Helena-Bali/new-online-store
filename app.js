5f910aef586c8f2aad7af2eb56af3538849e3959
Helena-Bali <elenabaltajs@gmail.com>
2023-02-18 20:06:11 +0300

    cross env

--- app.js
+++ app.js
@@ -33,5 +33,12 @@ server.get('/api/v1/rates', async (req, res) => {
         .catch(err => next(err))
 } )
 
+if (process.env.NODE_ENV === 'production&&') {
+    server.use('/', express.static(path.join(__dirname), 'client', 'build'))
+    server.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 server.listen(PORT,() => {console.log(`App is running on port ${PORT}`)})
